ac1ea3ed025b0d88fef6de4c6208c00c798fe8d0
chanakya.garg <chiraggarg025@gmail.com>
2020-07-12 15:24:56 +0530

    add contact list

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const port = 8000;
+const path=require('path')
+const app = express();
+
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+var contactList = [
+    {
+        name:'Chanakya',
+        phone:'11111111111'
+    },
+    {
+        name:'Tony Stark',
+        phone:'22222222222'
+    },
+    {
+        name:'Coding Ninjas',
+        phone:'3333333333'
+    }
+]
+app.get('/',function(req,res){
+    return res.render('index',{
+        title:'My Contact List',
+        contact_list:contactList
+    });
+})
+app.get('/playground',function(req,res){
+    return res.render('playing',{title:'My Play list'});
+})
+app.listen(port,function(err){
+
+    if(err){
+        console.log('error:',err);
+        return;
+    }
+    console.log('Server up and running on port:',port);
+})
+
+
